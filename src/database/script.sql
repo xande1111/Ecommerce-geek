17032b51398d3ca007f0e18043c40ceb2d763068
Ana Caroline Rieper Pereira <98720019+anarieper@users.noreply.github.com>
2023-03-05 23:30:58 -0300

    Update script.sql

--- src/database/script.sql
+++ src/database/script.sql
@@ -8,145 +8,117 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema ecommerce
+-- Schema desafio.final.gama
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `desafio.final.gama` ;
 
 -- -----------------------------------------------------
--- Schema ecommerce
+-- Schema desafio.final.gama
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `ecommerce` ;
+CREATE SCHEMA IF NOT EXISTS `desafio.final.gama` DEFAULT CHARACTER SET utf8mb3 ;
+USE `desafio.final.gama` ;
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`clients`
+-- Table `desafio.final.gama`.`tb_categories`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`clients` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `address` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `email` (`email` ASC) VISIBLE)
+CREATE TABLE IF NOT EXISTS `desafio.final.gama`.`tb_categories` (
+  `cat_id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(50) NOT NULL,
+  `cat_description` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`cat_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8mb3;
+
+CREATE UNIQUE INDEX `cat_id_UNIQUE` ON `desafio.final.gama`.`tb_categories` (`cat_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `cat_name_UNIQUE` ON `desafio.final.gama`.`tb_categories` (`cat_name` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`addresses`
+-- Table `desafio.final.gama`.`tb_users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`addresses` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `clients_id` INT NOT NULL,
-  `street` VARCHAR(255) NOT NULL,
-  `number` INT NOT NULL,
-  `complement` VARCHAR(255) NULL DEFAULT NULL,
-  `neighborhood` VARCHAR(255) NOT NULL,
-  `city` VARCHAR(255) NOT NULL,
-  `state` VARCHAR(255) NOT NULL,
-  `zip_code` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `clients_id` (`clients_id` ASC) VISIBLE,
-  CONSTRAINT `addresses_ibfk_1`
-    FOREIGN KEY (`clients_id`)
-    REFERENCES `ecommerce`.`clients` (`id`))
+CREATE TABLE IF NOT EXISTS `desafio.final.gama`.`tb_users` (
+  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(70) NOT NULL,
+  `user_email` VARCHAR(100) NOT NULL,
+  `user_password` VARCHAR(246) NOT NULL,
+  `user_type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8mb3;
+
+CREATE UNIQUE INDEX `id_usuarios_UNIQUE` ON `desafio.final.gama`.`tb_users` (`user_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `user_email_UNIQUE` ON `desafio.final.gama`.`tb_users` (`user_email` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`administrators`
+-- Table `desafio.final.gama`.`tb_orders`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`administrators` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `address` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `email` (`email` ASC) VISIBLE)
+CREATE TABLE IF NOT EXISTS `desafio.final.gama`.`tb_orders` (
+  `order_id` INT NOT NULL AUTO_INCREMENT,
+  `order_total` INT NOT NULL,
+  `created_at` DATE NOT NULL,
+  `updated_at` DATE NULL DEFAULT NULL,
+  `tb_users_user_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`order_id`),
+  CONSTRAINT `fk_tb_orders_tb_users1`
+    FOREIGN KEY (`tb_users_user_id`)
+    REFERENCES `desafio.final.gama`.`tb_users` (`user_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8mb3;
+
+CREATE UNIQUE INDEX `order_id_UNIQUE` ON `desafio.final.gama`.`tb_orders` (`order_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_tb_orders_tb_users1_idx` ON `desafio.final.gama`.`tb_orders` (`tb_users_user_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`categories`
+-- Table `desafio.final.gama`.`tb_products`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`categories` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `description` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `desafio.final.gama`.`tb_products` (
+  `product_id` INT NOT NULL AUTO_INCREMENT,
+  `product_name` VARCHAR(50) NOT NULL,
+  `product_photo` BLOB NOT NULL,
+  `product_description` VARCHAR(200) NOT NULL,
+  `tb_categories_cat_id` INT NOT NULL,
+  PRIMARY KEY (`product_id`),
+  CONSTRAINT `fk_tb_products_tb_categories`
+    FOREIGN KEY (`tb_categories_cat_id`)
+    REFERENCES `desafio.final.gama`.`tb_categories` (`cat_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8mb3;
+
+CREATE UNIQUE INDEX `product_id_UNIQUE` ON `desafio.final.gama`.`tb_products` (`product_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_tb_products_tb_categories_idx` ON `desafio.final.gama`.`tb_products` (`tb_categories_cat_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `ecommerce`.`products`
+-- Table `desafio.final.gama`.`tb_order_items`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`products` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `description` TEXT NULL DEFAULT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `stock` INT NOT NULL,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `desafio.final.gama`.`tb_order_items` (
+  `order_item_id` INT NOT NULL AUTO_INCREMENT,
+  `order_item_quantity` INT NOT NULL,
+  `tb_orders_order_id` INT NOT NULL,
+  `tb_products_product_id` INT NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  CONSTRAINT `fk_tb_order_items_tb_orders1`
+    FOREIGN KEY (`tb_orders_order_id`)
+    REFERENCES `desafio.final.gama`.`tb_orders` (`order_id`),
+  CONSTRAINT `fk_tb_order_items_tb_products1`
+    FOREIGN KEY (`tb_products_product_id`)
+    REFERENCES `desafio.final.gama`.`tb_products` (`product_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DEFAULT CHARACTER SET = utf8mb3;
 
+CREATE INDEX `fk_tb_order_items_tb_orders1_idx` ON `desafio.final.gama`.`tb_order_items` (`tb_orders_order_id` ASC) VISIBLE;
 
--- -----------------------------------------------------
--- Table `ecommerce`.`orders`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`orders` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `clients_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
-  `quantity` INT NOT NULL,
-  `total_price` DECIMAL(10,2) NOT NULL,
-  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  INDEX `clients_id` (`clients_id` ASC) VISIBLE,
-  INDEX `product_id` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `orders_ibfk_1`
-    FOREIGN KEY (`clients_id`)
-    REFERENCES `ecommerce`.`clients` (`id`),
-  CONSTRAINT `orders_ibfk_2`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `ecommerce`.`products` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `ecommerce`.`order_items`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`order_items` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `order_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
-  `quantity` INT NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `order_id` (`order_id` ASC) VISIBLE,
-  INDEX `product_id` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `order_items_ibfk_1`
-    FOREIGN KEY (`order_id`)
-    REFERENCES `ecommerce`.`orders` (`id`),
-  CONSTRAINT `order_items_ibfk_2`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `ecommerce`.`products` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE INDEX `fk_tb_order_items_tb_products1_idx` ON `desafio.final.gama`.`tb_order_items` (`tb_products_product_id` ASC) VISIBLE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
