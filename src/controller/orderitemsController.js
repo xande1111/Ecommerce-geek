d9a1e89c4b88113901dd5275f6c0d18b74161b45
Ana Caroline Rieper Pereira <98720019+anarieper@users.noreply.github.com>
2023-03-12 11:05:53 -0300

    Update orderitemsController.js

--- src/controller/orderitemsController.js
+++ src/controller/orderitemsController.js
@@ -2,58 +2,35 @@ const db = require("../database");
 const Orders = require("./orders");
 const Products = require("./products");
 
-const OrderItems = db.define("OrderItems", {
-  item_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+
+const OrderItemsController = {
+
+  async  createOrderItem(orderItemData) {
+    const orderItem = await OrderItems.create(orderItemData);
+    return orderItem;
   },
-  item_quantity: {
-    type: DataTypes.INTEGER,
+
+  async  getOrderItemById(itemId) {
+    const orderItem = await OrderItems.findByPk(itemId);
+    return orderItem;
   },
-  item_price: {
-    type: DataTypes.FLOAT,
+
+  async  updateOrderItem(itemId, updatedData) {
+    const orderItem = await getOrderItemById(itemId);
+    if (!orderItem) {
+      throw new Error("Order item not found");
+    }
+    const updatedOrderItem = await orderItem.update(updatedData);
+    return updatedOrderItem;
   },
-}, {
-  tableName: "tb_order_items",
-});
 
-// assssociações entre modelos
-OrderItems.belongsTo(Orders);
-OrderItems.belongsTo(Products);
-
-// funções de CRUD
-async function createOrderItem(orderItemData) {
-  const orderItem = await OrderItems.create(orderItemData);
-  return orderItem;
-}
-
-async function getOrderItemById(itemId) {
-  const orderItem = await OrderItems.findByPk(itemId);
-  return orderItem;
-}
-
-async function updateOrderItem(itemId, updatedData) {
-  const orderItem = await getOrderItemById(itemId);
-  if (!orderItem) {
-    throw new Error("Order item not found");
+  async  deleteOrderItem(itemId) {
+    const orderItem = await getOrderItemById(itemId);
+    if (!orderItem) {
+      throw new Error("Order item not found");
+    }
+    await orderItem.destroy();
   }
-  const updatedOrderItem = await orderItem.update(updatedData);
-  return updatedOrderItem;
-}
+  };
 
-async function deleteOrderItem(itemId) {
-  const orderItem = await getOrderItemById(itemId);
-  if (!orderItem) {
-    throw new Error("Order item not found");
-  }
-  await orderItem.destroy();
-}
-
-module.exports = {
-  OrderItems,
-  createOrderItem,
-  getOrderItemById,
-  updateOrderItem,
-  deleteOrderItem,
-};
+module.exports = OrderItemsController;
