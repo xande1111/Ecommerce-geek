14cfd46c4a0cc5a4b40b1b51df220c7a8af11a48
Ana Caroline Rieper Pereira <98720019+anarieper@users.noreply.github.com>
2023-03-12 11:09:36 -0300

    edita usersControllers para padronização

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,76 +1,80 @@
 const db = require("../database");
-const Users = require("../models/Users");
+const Users = require("../models/users");
 
-// Cria um novo usuário
-exports.createUser = async (req, res) => {
-try {
-const newUser = await Users.create({
-user_name: req.body.user_name,
-user_email: req.body.user_email,
-user_password: req.body.user_password,
-user_type: req.body.user_type,
-});
-res.status(201).json(newUser);
-} catch (err) {
-res.status(500).json({ message: err.message });
-}
-};
+const usersController = {
 
-// Obtém todos os usuários
-exports.getUsers = async (req, res) => {
-try {
-const users = await Users.findAll();
-res.status(200).json(users);
-} catch (err) {
-res.status(500).json({ message: err.message });
-}
-};
+    async createUser(req, res) {
+    try {
+    const newUser = await Users.create({
+    user_name: req.body.user_name,
+    user_email: req.body.user_email,
+    user_password: req.body.user_password,
+    user_type: req.body.user_type,
+    });
+    res.status(201).json(newUser);
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+    }
+    },
 
-// Obtém um usuário pelo ID
-exports.getUserById = async (req, res) => {
-try {
-const user = await Users.findByPk(req.params.id);
-if (user === null) {
-res.status(404).json({ message: "Usuário não encontrado" });
-} else {
-res.status(200).json(user);
-}
-} catch (err) {
-res.status(500).json({ message: err.message });
-}
-};
 
-// Atualiza um usuário pelo ID
-exports.updateUser = async (req, res) => {
-try {
-const user = await Users.findByPk(req.params.id);
-if (user === null) {
-res.status(404).json({ message: "Usuário não encontrado" });
-} else {
-const updatedUser = await user.update({
-user_name: req.body.user_name,
-user_email: req.body.user_email,
-user_password: req.body.user_password,
-user_type: req.body.user_type,
-});
-res.status(200).json(updatedUser);
-}
-} catch (err) {
-res.status(500).json({ message: err.message });
-}
-};
+    async getUsers(req, res) {
+    try {
+    const users = await Users.findAll();
+    res.status(200).json(users);
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+    }
+    },
 
-// Deleta um usuário pelo ID
-exports.deleteUser = async (req, res) => {
-try {
-const user = await Users.findByPk(req.params.id);
-if (user === null) {
-res.status(404).json({ message: "Usuário não encontrado" });
-} else {
-await user.destroy();
-res.status(204).json();
+
+    async getUserById(req, res) {
+    try {
+    const user = await Users.findByPk(req.params.id);
+    if (user === null) {
+    res.status(404).json({ message: "Usuário não encontrado" });
+    } else {
+    res.status(200).json(user);
+    }
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+    }
+    },
+
+
+    async updateUser(req, res) {
+    try {
+    const user = await Users.findByPk(req.params.id);
+    if (user === null) {
+    res.status(404).json({ message: "Usuário não encontrado" });
+    } else {
+    const updatedUser = await user.update({
+    user_name: req.body.user_name,
+    user_email: req.body.user_email,
+    user_password: req.body.user_password,
+    user_type: req.body.user_type,
+    });
+    res.status(200).json(updatedUser);
+    }
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+    }
+    },
+
+
+    async deleteUser(req, res){
+    try {
+    const user = await Users.findByPk(req.params.id);
+    if (user === null) {
+    res.status(404).json({ message: "Usuário não encontrado" });
+    } else {
+    await user.destroy();
+    res.status(204).json();
+    }
+    } catch (err) {
+    res.status(500).json({ message: err.message });
+    }
+    }
 }
-} catch (err) {
-res.status(500).json({ message: err.message });
-}
-};
+
+module.exports = usersController;
