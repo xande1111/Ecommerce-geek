2b955bbf172cf7033b9c4a8453353be23362c7e7
Ana Caroline Rieper Pereira <98720019+anarieper@users.noreply.github.com>
2023-03-12 11:10:31 -0300

    adiciona routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,40 @@
-const express = require('express');
+const express = require("express");
 
+const productsController = require("../controllers/productsController");
+const categoriesController = require("../controllers/categoriesController");
+const orderitemsController = require("../controllers/orderitemsController");
+const ordersController = require("../controllers/ordersController");
+const usersController = require('../controllers/usersController');
 const routes = express.Router();
 
-//Controllers
-const productController = require('../controller/productsController');
-const categoriesContrller = require('../controller/categoriesController');
-const orderitemsController = require('../controller/orderitemsController');
-const ordersController = require('../controller/ordersController');
-const usersController = require('../controller/usersController');
+routes.get("/", (req, res)=> {
+    res.send("Bem vindo à nossa loja");
+});
 
-//Login e Middlewares
+routes.get("/order-items/:id", orderitemsController.getOrderItemById);
+routes.post("/order-items", orderitemsController.createOrderItem);
+routes.put("/order-items", orderitemsController.updateOrderItem);
+routes.delete("order-items", orderitemsController.deleteOrderItem);
 
+routes.get("users", usersController.getUsers);
+routes.get("users/:id", usersController.getUserById);
+routes.post("users", usersController.createUser);
+routes.put("users", usersController.updateUser);
+routes.delete("users", usersController.deleteUser);
 
+routes.get("orders", ordersController.getAllOrders);
+routes.post("orders", ordersController.createOrder);
 
+routes.get("categories", categoriesController.getCategories);
+routes.get("categories/:id", categoriesController.getCategoriesById);
+routes.post("categories", categoriesController.createCategories);
+routes.put("categories", categoriesController.updateCategories);
+routes.delete("categories", categoriesController.deleteCategories);
 
-module.exports = routes;
+routes.get("products", productsController.getProducts);
+routes.get("products/:id", productsController.getProductById);
+routes.post("products", productsController.createProduct);
+routes.put("products", productsController.updateProduct);
+routes.delete("products", productsController.deleteProduct);
+
+module.exports = routes; 
